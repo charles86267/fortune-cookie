afb16f18789fbd31588605d624b3628ac0f42051
Lee <300086267@fvtc.edu>
2025-05-12 14:38:58 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const getFortune = require('./getFortune')
+
+const message = getFortune()
+console.log("Your Fortune: " + message)
---
+++ getFortune.js
@@ -0,0 +1,9 @@
+const fortunes = require('../fortunes.json')
+
+function getFortune() {
+    const randomIndex = Math.floor(Math.random() * fortunes.length)
+    const randomMssg = fortunes[randomIndex]
+    return randomMssg
+}
+
+module.exports = getFortune
